3ecf65c171e36b2d22c3eca21d66b9c0f3918a63
dino <dino.tatsu@mail.ru>
2017-10-16 19:24:30 +0300

    Проект есть

---
+++ TMproj/TMproj/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMproj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int q = 7;
+            int w = 5;
+            int sum = q + w;
+            ViewBag.q = q;
+            ViewBag.w = 5;
+            ViewBag.sum = sum;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
