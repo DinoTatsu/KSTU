afee2c61902166f87b523bdcf9defa47758a6135
Sedokina <sedokina52@gmail.com>
2017-10-17 22:18:18 +0300

    Подключение к БД + вывод списка notes, через Entity Framework

---
+++ TMproj/TMproj/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMproj.Models
+{
+    public class Note
+    {
+        public int noteID { get; set; }
+        public DateTime created { get; set; }
+        public string title { get; set; }
+        public string article { get; set; }
+    }
+}
