525cba22c2127ad3a1c91cb4b0a4dbe602d17764
Sedokina <sedokina52@gmail.com>
2017-10-25 10:02:36 +0300

    Представления, шаблоны

---
+++ onemp/onemapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return HttpResponse("You're looking at %s" % post.title)
+    #return render(request, 'detail.html', {'post': post})
